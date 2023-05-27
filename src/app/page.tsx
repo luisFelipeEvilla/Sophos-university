2a6053edf4dfbbfab458871677c0adc632a427e1
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-05-27 13:23:34 -0500

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-
+    <main className="flex min-h-screen flex-col items-center justify-between w-full">
+      <div className='mt-20 text-center'>
+        <h1 className='text-4xl font-bold'>Welcome to Sophos University</h1>
+        <p className='text-lg mt-6'>Here you can find all the information about the university</p>
+      </div>
     </main>
   )
 }
