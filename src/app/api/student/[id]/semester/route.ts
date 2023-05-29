6f55d356e2285b6a48b371c87dc0e71320eac794
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-05-28 20:10:49 -0500

    error handler for student enroll

--- src/app/api/student/[id]/semester/route.ts
+++ src/app/api/student/[id]/semester/route.ts
@@ -1,19 +1,7 @@
+import { serverRequest } from "@/utils/requests";
 import { NextResponse } from "next/server";
 
 export async function POST (req: Request, {params}: any) {
-    const {id} = params;
-
     const data = await req.json();
-
-    const res = await fetch(`${process.env.API_BASE_URL}/student/${id}/semester`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-
-    const result = await res.json();
-
-    return NextResponse.json(result, {status: res.status})
+    return await serverRequest(`student/${params.id}/semester`, 'POST', data);    
 }
